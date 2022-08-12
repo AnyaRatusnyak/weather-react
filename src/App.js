4e27b16e740001cab3d96ed84102dce70814bfdf
AnyaRatusnyak <anyaaboyko@gmail.com>
2022-08-12 23:07:24 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,27 +5,34 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <Weather />
-      <br />
-      <a
-        href="https://github.com/AnyaRatusnyak/weather-react.git"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Open-source code
-      </a>
-      ,by{" "}
-      <a
-        href="https://github.com/AnyaRatusnyak"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Anna Ratushnyak
-      </a>{" "}
-      from{" "}
-      <a href="https://www.shecodes.io" target="_blank" rel="noreferrer">
-        SheCodes
-      </a>
+      <div className="container">
+        <Weather />
+        <footer>
+          <a
+            href="https://github.com/AnyaRatusnyak/weather-react.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code
+          </a>
+          ,by{" "}
+          <a
+            href="https://github.com/AnyaRatusnyak"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Anna Ratushnyak
+          </a>{" "}
+          from{" "}
+          <a
+            href="https://www.shecodes.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            SheCodes
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
